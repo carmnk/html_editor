4675e9d4a45d912ebc5ceebd35165dcf0196c654
carmnk <carso.menk@gmail.com>
2024-01-16 18:06:55 +0100

    change root

--- src/HtmlEditor/defs/baseHtmlElements.ts
+++ src/HtmlEditor/defs/baseHtmlElements.ts
@@ -1,36 +1,36 @@
 import { HtmlEditorElementType } from "../EditorState";
 
 export const baseHtmlDocument: HtmlEditorElementType[] = [
+  // {
+  //   id: "html_root",
+  //   type: "html",
+  //   attributes: {
+  //     style: {
+  //       height: "100%",
+  //     },
+  //   },
+  //   children: [
+  //     {
+  //       id: "head_root",
+  //       type: "head",
+  //       // children: [
+  //       // ]
+  //       attributes: {
+  //         // style: {
+  //         //   backgroundColor: "red",
+  //         // },
+  //       }
+  //     },
   {
-    id: "html_root",
-    type: "html",
+    id: "app_root",
+    type: "div",
+    children: [],
     attributes: {
       style: {
         height: "100%",
       },
     },
-    children: [
-      {
-        id: "head_root",
-        type: "head",
-        // children: [
-        // ]
-        attributes: {
-          // style: {
-          //   backgroundColor: "red",
-          // },
-        }
-      },
-      {
-        id: "body_root",
-        type: "body",
-        children: [],
-        attributes: {
-          style: {
-            height: "100%",
-          },
-        },
-      },
-    ],
   },
+  //   ],
+  // },
 ];
